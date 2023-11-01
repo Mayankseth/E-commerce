b67e24bee9a785a19879705a7f9d3ba7078399ad
Mayankseth <sethmayank33@gmail.com>
2023-11-01 18:57:16 +0530

    Popular items

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,4 +1,4 @@
-import p1_img from './product_1.png'
+import p1_product_i1 from './product_11.png'
 import p2_img from './product_2.png'
 import p3_img from './product_3.png'
 import p4_img from './product_4.png'
@@ -7,7 +7,7 @@ let data_product = [
   {
     id:1,
     name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image:p1_img,
+    image:p1_product_i1,
     new_price:50.00,
     old_price:80.50,
   },
