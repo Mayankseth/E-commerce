3f0c29c2ff72ca81a9138f0cb357b79426c27851
Mayankseth <sethmayank33@gmail.com>
2023-11-08 20:33:42 +0530

    new changes

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Offers.css'
+import exclusive_image from '../Assets/exclusive_image.png'
 const Offers = () => {
   return (
     <div className='offers'>
@@ -11,7 +12,7 @@ const Offers = () => {
             
         </div>
         <div className="offers-right">
-
+            <img src={exclusive_image} alt="" />
         </div>
     </div>
   )
